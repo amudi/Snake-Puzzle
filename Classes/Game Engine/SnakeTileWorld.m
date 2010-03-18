40031e8003fce812759ee2a1d72c9a5957b261d1
Amudi Sebastian <amudi@amudi-sebastians-macbook-pro.local>
2010-03-18 23:15:32 +0800

    updated SnakeTileWorld to calculate location in tile

--- Classes/Game Engine/SnakeTileWorld.m
+++ Classes/Game Engine/SnakeTileWorld.m
@@ -1,19 +1,19 @@
 //
-//  TileWorld.m
-//  DCrawler
+//  SnakeTileWorld.m
+//  Snake Puzzle
 //
-//  Created by Amudi Sebastian on 3/14/10.
+//  Created by Amudi Sebastian on 3/18/10.
 //  Copyright 2010 amudi.org. All rights reserved.
 //
 
-#import "TileWorld.h"
+#import "SnakeTileWorld.h"
 #import "Tile.h"
 #import "Sprite.h"
 #import "ResourceManager.h"
 #import "Entity.h"
 #import "pointmath.h"
 
-@implementation TileWorld
+@implementation SnakeTileWorld
 
 @synthesize worldWidth;
 @synthesize worldHeight;
@@ -31,10 +31,47 @@
 	}
 }
 
-- (TileWorld *)initWithFrame:(CGRect)frame {
+- (SnakeTileWorld *)initWithFrame:(CGRect)frame {
 	[super init];
 	view = frame;
 	return self;
+}
+
+- (void)loadLevel:(NSString *)levelFilename withTiles:(NSString *)imageFilename {
+	NSData *fileData = [gResManager getBundledData:levelFilename];
+	NSString *dush = [[NSString alloc] initWithData:fileData encoding:NSASCIIStringEncoding];
+	
+	NSArray *rows = [dush componentsSeparatedByString:@"\n"];
+	int rowIndex = 0;
+	NSArray *wh = [[rows objectAtIndex:rowIndex] componentsSeparatedByString:@"x"];
+	int width = [[wh objectAtIndex:0] intValue];
+	int height = [[wh objectAtIndex:1] intValue];
+	[self allocateWidth:width height:height];
+	++rowIndex;
+	
+	NSLog(@"loadlevel dimension %d%d", width, height);
+	
+	for (int y = 0; y < worldHeight; ++y) {
+		NSArray *row = [[rows objectAtIndex:rowIndex] componentsSeparatedByString:@","];
+		for (int x = 0; x < worldWidth; ++x) {
+			int tile = [[row objectAtIndex:x] intValue];
+			int tx = (tile * (int)TILE_SIZE) % kMaxTextureSize;
+			int row = ((tile * TILE_SIZE) - tx) / kMaxTextureSize;
+			int ty = row * TILE_SIZE;
+			[tiles[x][worldHeight - y - 1] initWithTexture:imageFilename withFrame:CGRectMake(tx, ty, TILE_SIZE, TILE_SIZE)];
+		}
+		++rowIndex;
+	}
+	++rowIndex;
+	for (int y = 0; y < worldHeight; ++y) {
+		NSArray *row = [[rows objectAtIndex:rowIndex] componentsSeparatedByString:@","];
+		for (int x = 0; x < worldWidth; ++x) {
+			int flags = [[row objectAtIndex:x] intValue];
+			tiles[x][worldHeight - y - 1]->flags = (PhysicsFlags)flags;
+		}
+		++rowIndex;
+	}
+	[dush release];
 }
 
 - (void)draw {
@@ -88,6 +125,59 @@
 	}
 }
 
+- (void)addEntity:(Entity *)entity {
+	if (!entities) {
+		entities = [[NSMutableArray alloc] init];
+	}
+	[entity setWorld:self];
+	[entities addObject:entity];
+}
+
+- (void)removeEntity:(Entity *)entity {
+	[entities removeObject:entity];
+}
+
+
+// used when tapping an area of the screen to move player
+- (CGPoint)worldPosition:(CGPoint)screenPosition {
+	CGFloat xoff, yoff;
+	// get actual world position (in pixel)
+	if (landscape) {
+		xoff = cameraX + view.origin.x - view.size.height / 2;
+		yoff = cameraY + view.origin.y - view.size.width / 2;
+	} else {
+		xoff = cameraX + view.origin.x - view.size.width / 2;
+		yoff = cameraY + view.origin.y - view.size.height / 2;
+	}
+	
+	// convert pixel position to tile position
+	int xOffInt = (int) xoff;
+	int yOffInt = (int) yoff;
+	
+	// calculate tile offset in tile
+	int xTileOff = xOffInt / TILE_SIZE + (xOffInt % TILE_SIZE > 0 ? 1 : 0);
+	int yTileOff = yOffInt / TILE_SIZE + (yOffInt % TILE_SIZE > 0 ? 1 : 0);
+	
+	// calculate touch position in tile
+	int xScreenPosInt = (int)screenPosition.x;
+	int yScreenPosInt = (int)screenPosition.y;
+	int xTilePos = (xScreenPosInt / TILE_SIZE) + (xScreenPosInt % TILE_SIZE > 0 ? 1 : 0);
+	int yTilePos = (yScreenPosInt / TILE_SIZE) + (yScreenPosInt % TILE_SIZE > 0 ? 1 : 0);
+	
+	NSLog(@"tap at tile (%d, %d)", xTilePos, yTilePos);
+	return CGPointMake(xTileOff + xTilePos, yTileOff + yTilePos);
+}
+
+// used by entities to get the tile they are standing over
+- (Tile *)tileAt:(CGPoint)worldPosition {
+	int x = worldPosition.x / TILE_SIZE;
+	int y = worldPosition.y / TILE_SIZE;
+	if (worldPosition.x < 0 || worldPosition.y < 0 || x >= worldWidth || y >= worldHeight) {
+		return nil;
+	}
+	return tiles[x][y];
+}
+
 - (void)setCamera:(CGPoint)position {
 	cameraX = position.x;
 	cameraY = position.y;
@@ -120,78 +210,6 @@
 		}
 	}
 	//NSLog(@"Camera after: (%d, %d)", cameraX, cameraY);
-}
-
-- (void)loadLevel:(NSString *)levelFilename withTiles:(NSString *)imageFilename {
-	NSData *fileData = [gResManager getBundledData:levelFilename];
-	NSString *dush = [[NSString alloc] initWithData:fileData encoding:NSASCIIStringEncoding];
-	
-	NSArray *rows = [dush componentsSeparatedByString:@"\n"];
-	int rowIndex = 0;
-	NSArray *wh = [[rows objectAtIndex:rowIndex] componentsSeparatedByString:@"x"];
-	int width = [[wh objectAtIndex:0] intValue];
-	int height = [[wh objectAtIndex:1] intValue];
-	[self allocateWidth:width height:height];
-	++rowIndex;
-	
-	NSLog(@"loadlevel dim %d%d", width, height);
-	
-	for (int y = 0; y < worldHeight; ++y) {
-		NSArray *row = [[rows objectAtIndex:rowIndex] componentsSeparatedByString:@","];
-		for (int x = 0; x < worldWidth; ++x) {
-			int tile = [[row objectAtIndex:x] intValue];
-			int tx = (tile * (int)TILE_SIZE) % kMaxTextureSize;
-			int row = ((tile * TILE_SIZE) - tx) / kMaxTextureSize;
-			int ty = row * TILE_SIZE;
-			[tiles[x][worldHeight - y - 1] initWithTexture:imageFilename withFrame:CGRectMake(tx, ty, TILE_SIZE, TILE_SIZE)];
-		}
-		++rowIndex;
-	}
-	++rowIndex;
-	for (int y = 0; y < worldHeight; ++y) {
-		NSArray *row = [[rows objectAtIndex:rowIndex] componentsSeparatedByString:@","];
-		for (int x = 0; x < worldWidth; ++x) {
-			int flags = [[row objectAtIndex:x] intValue];
-			tiles[x][worldHeight - y - 1]->flags = (PhysicsFlags)flags;
-		}
-		++rowIndex;
-	}
-	[dush release];
-}
-
-- (void)addEntity:(Entity *)entity {
-	if (!entities) {
-		entities = [[NSMutableArray alloc] init];
-	}
-	[entity setWorld:self];
-	[entities addObject:entity];
-}
-
-- (void)removeEntity:(Entity *)entity {
-	[entities removeObject:entity];
-}
-
-// used when tapping an area of the screen to move player
-- (CGPoint)worldPosition:(CGPoint)screenPosition {
-	CGFloat xoff, yoff;
-	if (landscape) {
-		xoff = cameraX + view.origin.x - view.size.height / 2;
-		yoff = cameraY + view.origin.y - view.size.width / 2;
-	} else {
-		xoff = cameraX + view.origin.x - view.size.width / 2;
-		yoff = cameraY + view.origin.y - view.size.height / 2;
-	}
-	return CGPointMake(xoff + screenPosition.x, yoff + screenPosition.y);
-}
-
-// used by entities to get the tile they are standing over
-- (Tile *)tileAt:(CGPoint)worldPosition {
-	int x = worldPosition.x / TILE_SIZE;
-	int y = worldPosition.y / TILE_SIZE;
-	if (worldPosition.x < 0 || worldPosition.y < 0 || x >= worldWidth || y >= worldHeight) {
-		return nil;
-	}
-	return tiles[x][y];
 }
 
 - (BOOL)walkable:(CGPoint)point {
